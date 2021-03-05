4f9e2f9496a0de1718aa28f55797244b01ef7895
Kelly Cook <kellyrcook1980@gmail.com>
2021-03-04 20:08:23 -0700

    show all employee table working, create new employee working except manager portion

--- index.js
+++ index.js
@@ -1,14 +1,103 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const { registerPrompt } = require('inquirer');
 
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
-    database: 'employeeTrackerDB',
+    password: 'WeeRiden1!',
+    database: 'employeetrackerdb',
+});
+
+const newEmp = () => {
+    inquirer
+     .prompt([
+     {
+        name: 'first_name',
+        type: 'input',
+        message: 'New employee first name?',
+     },
+     {
+        name: 'last_name',
+        type: 'input',
+        message: 'New employee last name?',
+     },
+     {
+        name: 'role',
+        type: 'list',
+        message: 'New employee title?',
+        choices: ['Sales Manager', 'Sales Associate', 'CFO', 'Accountant', 'COO', 'Marketing', 'Contract Coordinator']
+     },
+     {
+        name: 'manager',
+        type: 'input',
+        message: 'Manager for new employee?',
+     },
+     {
+        name: 'salary',
+        type: 'input',
+        message: 'New employee salary',
+     },
+    ]).then((response) => {
+        let role;
+        let department;
+         console.log(response)
+        console.log('Inserting New Employee Information\n');
+            if(response.role === "Sales Manager"){
+                role = 1
+                department = 1
+            }
+            else if(response.role === "Sales Associate"){
+                role = 2
+                department = 1
+            }
+            else if(response.role === "CFO"){
+                role = 3
+                department = 2
+            }
+            else if(response.role === "Accountant"){
+                role = 4
+                department = 2
+            }
+            else if(response.role === "COO"){
+                role = 5
+                department = 3
+            }
+            else if(response.role === "Marketing"){
+                role = 6
+                department = 3
+            }
+            else if(response.role === "Contract Coordinator"){
+                role = 7
+                department = 3
+            }
+            //search manager name and return manager empID
+        connection.query(
+           
+            'INSERT INTO employee SET ?',
+            {
+                first_name: response.first_name,
+                last_name: response.last_name,
+                role_id: role,
+                
+            },
+            (err,res)=> {
+                if (err) throw err;
+                console.log(`${res.affectedRows} product inserted!\n`)
+            }
+        )
+       
+        start();
+     })
+}
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    start();
 });
 
 const start = () => {
@@ -20,10 +109,11 @@ const start = () => {
             choices: ['View current employees', 'View current employees by department', 'View current employees by role', 'Enter new employee', 'Remove current employee', 'Update current employee role', 'Update current employee manager','I am finished']
         }).then((response) => {
             if(response.action === 'View current employees'){
-                //sql query and display table
+                allEmp();
             }
-            else if (response.action === 'View current employees by department'){
+            else if (response.action === 'Enter new employee'){
                 //sql query and display table
+                newEmp();
             }
             else if (response.action === 'View current employees by role'){
                 //sql query and display table
@@ -50,25 +140,23 @@ const start = () => {
 
 //function to display all employees
 const allEmp = () => {
-    connection.query('SELECT * FROM employee JOIN role on employee.role_id = role.id JOIN role on role.department_id = department.id', (err, res) => {
+    connection.query('SELECT first_name,last_name,manager_id,title,salary,name FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id;', (err, res) => {
         if (err) throw err;
-        console.table(res);
-        // console.table('Current Employees', [
-        //     {
-        //       name: 'foo',
-        //       age: 10
-        //     }, {
-        //       name: 'bar',
-        //       age: 20
-        //     }
-        //   ]);
+        
+        console.table('Current Employees', res);
+        start();
     });
 
 };
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId}`);
-    start();
-});
+// const empByDep = () => {
+//     connection.query('SELECT first_name,last_name,manager_id,title,salary,name FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id;', (err, res) => {
+//         if (err) throw err;
+        
+//         console.table('Current Employees', res);
+//         start();
+//     });
+// }
+
+
 
